2e0cd979da68ddc5af8bc5b1c346b5a596fd7970
MAZE-wnglk <wnglk62@a-maze.ai>
2023-12-29 13:19:47 +0900

    feat: :sparkles: 데이터 나열 포맷 변경

--- src/App.js
+++ src/App.js
@@ -23,13 +23,23 @@ function DataFetchingComponent() {
 
   // 화면에 데이터 리스트를 나열
   return (
-    <div>
-      <h1>받아온 데이터</h1>
-      <ul>
-        {dataList.map((item, index) => (
-          <li key={index}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
+    <div style={{padding: 20}}>
+      <h1>2024년 12월 29일 매장 내 실시간 현황</h1>
+      {dataList.map((item, index) => (
+        <div key={index} style={{ marginBottom: '20px', padding: '10px' }}>
+          <p><strong>시각:</strong> {item.timestamp}</p>
+          <p><strong>현재 매장 내 고객 목록:</strong> 'g_1', 'g_2', 'g_3', 'g_4'</p>
+          <p><strong>이벤트 발생 목록</strong></p>
+          {console.log(item.data.f)}
+          <ul>
+            <li>{item.type === 1 ? 
+                      `${item.data.user_id}가 ${item.data.zone_id}에 관심을 보이고 있습니다.`:
+                      `${item.data.user_id}가 ${item.data.from_zone_id}에서 ${item.data.to_zone_id}로 이동했습니다.`}
+                      </li>
+                      </ul>
+          </div>
+          ))}
+      
     </div>
   );
 }
